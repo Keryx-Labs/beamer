5fb1b1f5e7624be432a18bb0e31c38e04b3b8611
devsheke <166947651+devsheke@users.noreply.github.com>
2025-03-03 14:21:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<p>
+    <h1>Beamer</h1>
+</p>
+
+<p>
+    <a aria-label="License" href="https://github.com/keryx-labs/beamer/blob/main/LICENSE"><img alt="" src="https://img.shields.io/github/license/Keryx-Labs/beamer?style=flat"></a>
+</p>
+
+Beamer is a flexible bulk mailer built for power users who need efficient email delivery at scale.
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
+      devShell = with pkgs;
+        mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-analyzer
+            rust-bin.stable.latest.default
+          ];
+        };
+    });
+}
